abb3a77003fa94567e1dea6a6d105800ee50b6a3
saturnali <119850413+saturnali@users.noreply.github.com>
2020-11-29 11:32:14 +0000

    Release 0.0.17

--- nlp_proyect/run.py
+++ nlp_proyect/run.py
@@ -2,3 +2,8 @@
 from src.task_1 import movies_reviews
 from src.task_2 import model_trainer
 from src.task_3 import translate_blue
+
+
+if __name__ == '__main__':
+    print("Task 1: Out of the Box Sentiment Analysis ")
+    task_1 = movies_reviews()
