05cd035db8bd44fcd5bc7f7a4b4583eb762dba34
saturnali <119850413+saturnali@users.noreply.github.com>
2020-11-29 21:43:19 +0000

    Adding license headers.

--- nlp_proyect/run.py
+++ nlp_proyect/run.py
@@ -7,3 +7,8 @@ from src.task_3 import translate_blue
 if __name__ == '__main__':
     print("Task 1: Out of the Box Sentiment Analysis ")
     task_1 = movies_reviews()
+    task_1.main()
+    print("-----------END------------")
+    print("Task 2: Take a basic, pretrained NER model, and train further on a task-specific dataset")
+    samples_train = 300
+    samples_test = 30
