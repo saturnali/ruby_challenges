fddeaf0990e6ef8e39957bc6a3e004c7933b1598
saturnali <119850413+saturnali@users.noreply.github.com>
2021-04-04 20:37:14 +0000

    Issue fixes

--- nlp_proyect/src/task_1.py
+++ nlp_proyect/src/task_1.py
@@ -3,4 +3,4 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipe
 
 class movies_reviews():
     def __init__(self):
-        #Preparing model and t
+        #Preparing model and token
