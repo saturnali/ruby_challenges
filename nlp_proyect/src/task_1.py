c1ef4252e1e16eec46dab6233e72b8804adfc2c7
saturnali <119850413+saturnali@users.noreply.github.com>
2021-05-05 15:57:33 +0000

    2004-01-17  Andrew Cagney  <cagney@redhat.com>

--- nlp_proyect/src/task_1.py
+++ nlp_proyect/src/task_1.py
@@ -5,4 +5,4 @@ class movies_reviews():
     def __init__(self):
         #Preparing model and tokenizer
         self.model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment")
-        self.tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/tw
+        self.tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-ro
