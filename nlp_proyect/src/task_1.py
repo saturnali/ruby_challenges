18cdff99968b24fda519c8e19e258a1ac40ac1ff
saturnali <119850413+saturnali@users.noreply.github.com>
2022-04-02 11:36:10 +0000

    Minor docs update

--- nlp_proyect/src/task_1.py
+++ nlp_proyect/src/task_1.py
@@ -16,4 +16,4 @@ class movies_reviews():
 
         for i, review in enumerate(reviews): 
             inputs = review
-            output = self.classifier
+            output = self.classifier(inp
