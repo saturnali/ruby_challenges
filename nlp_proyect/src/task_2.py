7970619772db22b2fb9f526021ada862d3bd408a
saturnali <119850413+saturnali@users.noreply.github.com>
2022-10-28 06:06:50 +0000

    HTML cleanup

--- nlp_proyect/src/task_2.py
+++ nlp_proyect/src/task_2.py
@@ -2,3 +2,6 @@
 from datasets import load_dataset
 from transformers import AutoTokenizer
 from transformers import DefaultDataCollator
+import tensorflow as tf
+from transformers import TFAutoModelForSequenceClassification
+from matplotlib import pyplot as plt
