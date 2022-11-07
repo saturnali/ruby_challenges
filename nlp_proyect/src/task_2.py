db886f258a0b5b3f38126451641a0cac195643cd
saturnali <119850413+saturnali@users.noreply.github.com>
2022-11-07 13:04:40 +0000

    Updated docstring.

--- nlp_proyect/src/task_2.py
+++ nlp_proyect/src/task_2.py
@@ -40,3 +40,4 @@ class model_trainer():
       self.tf_validation_dataset = self.small_eval_dataset.to_tf_dataset(
           columns=["attention_mask", "input_ids"],
           label_cols=["labels"],
+          shuffle=False,
