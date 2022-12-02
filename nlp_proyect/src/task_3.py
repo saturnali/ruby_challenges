ea4af4fe8318c76a74882377822f91c3b4dc02c8
saturnali <119850413+saturnali@users.noreply.github.com>
2022-12-02 05:25:16 +0000

    Translated using Weblate (Asturian)

--- nlp_proyect/src/task_3.py
+++ nlp_proyect/src/task_3.py
@@ -4,3 +4,6 @@ import requests, uuid
 from nltk.translate.bleu_score import sentence_bleu
 import statistics
 import os
+
+class translate_blue():
+  def __init__(self, lang1_set, lang2_set, lang_from, lang_to, key_azure, region_azure, gcp_keys_json_name):
