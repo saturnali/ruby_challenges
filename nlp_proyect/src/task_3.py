19ab78b4e792d78485070df96b2ebd2b309c3471
saturnali <119850413+saturnali@users.noreply.github.com>
2022-12-13 20:47:48 +0000

    xHCI: handle command after aborting the command ring

--- nlp_proyect/src/task_3.py
+++ nlp_proyect/src/task_3.py
@@ -30,3 +30,5 @@ class translate_blue():
 
     headers = {
         'Ocp-Apim-Subscription-Key': self.cod_key,
+        # location required if you're using a multi-service or regional (not global) resource.
+        'Ocp-Apim-Subscription-Region': self.cod_region,
