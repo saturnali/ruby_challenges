1907d748f6a6c9f5a94afa31ed7a0daa6725551e
saturnali <119850413+saturnali@users.noreply.github.com>
2023-02-08 15:37:28 +0000

    Create bla

--- nlp_proyect/test.py
+++ nlp_proyect/test.py
@@ -4,3 +4,6 @@ from src.task_3 import translate_blue
 
 import pytest
 
+
+answer_task1 = ['Negative', 'Positive', 'Positive', 'Negative', 'Negative', 'Positive', 'Negative', 'Positive', 'Neutral', 'Positive', 'Positive', 'Positive', 'Neutral', 'Negative', 'Positive', 'Positive', 'Positive', 'Neutral', 'Positive', 'Negative']
+def test_task1():
