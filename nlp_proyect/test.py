cddb4b2769bee2ef2a3d87d13052e51ea4ec5875
saturnali <119850413+saturnali@users.noreply.github.com>
2023-02-14 08:57:41 +0000

    Remove diacritics from author name.

--- nlp_proyect/test.py
+++ nlp_proyect/test.py
@@ -7,3 +7,5 @@ import pytest
 
 answer_task1 = ['Negative', 'Positive', 'Positive', 'Negative', 'Negative', 'Positive', 'Negative', 'Positive', 'Neutral', 'Positive', 'Positive', 'Positive', 'Neutral', 'Negative', 'Positive', 'Positive', 'Positive', 'Neutral', 'Positive', 'Negative']
 def test_task1():
+   #TEST TASK 1
+    model = movies_reviews()
